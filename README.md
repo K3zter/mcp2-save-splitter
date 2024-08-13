4f06767a98c26ae50d36b67cb3c11dc9d06e2d40
K3zter <7936822+K3zter@users.noreply.github.com>
2024-08-13 21:05:04 +0100

    Improve usability, update README

--- README.md
+++ README.md
@@ -1,14 +1,55 @@
 # MCP2 Save Splitter
-Simple powershell script for generating VMC .mc2 files for use with the Memcard Pro 2's GameID functionality.
+Simple PowerShell script for generating and modifying VMC .mc2 files for use with the Memcard Pro 2's GameID functionality.
 
 This script relies on the application MyMC v2.6.g2 by gastroman. Download the file from [this page](https://sourceforge.net/projects/mymc-opl/files/) and place the contents of `mymc_2.6.g2.dist.7z` in the `mymc` folder. The file `mymc.exe` should be in the root of the folder.
 
+In order to run this script, you will need to update your PowerShell execution policy to run unsigned scripts, this can be done by running PowerShell as administrator and running the following command:
+
+    Set-ExecutionPolicy unrestricted
+
+If you wish to reverse this change you can run this command in PowerShell as administrator:
+
+    Set-ExecutionPolicy restricted
+
+The script currently performs three separate functions:
+ - **Save Splitting**: Take a collection of PS2 virtual memory card (VMC) files and individual save files from the `import` folder and generate separate MCP2 compatible VMC files in the `export` folder
+ - **Import PSU to existing VMCs**: Import a specified `.psu` file to every single `.mc2` file (MCP2 VMC) in the `existing_cards` folder
+ - **Remove folder from existing VMCs**: Remove a given named folder from every single `.mc2` file (MCP2 VMC) in the `existing_cards` folder
+
+The latter two functions are intended for the purposes of adding an OPL IGR command to every VMC - but it is quite flexible and it may be useful for other things. I don't know what! Instructions for each of these functions are outlined below.
+
+# IMPORTANT: PLEASE ENSURE ALL YOUR SAVE FILES ARE BACKED UP BEFORE USING THIS TOOL TO PREVENT RISK OF SAVE FILE LOSS OR CORRUPTION
+
+## Save Splitting
 In order to process `.psv` files, the tool PSV Save Converter is required. Download the Win64 build from [this page](https://github.com/bucanero/psv-save-converter/releases/tag/v1.2.1) and place `psv-converter-win.exe` in the `psv-converter` folder. 
 
 Place any files you wish to use with the Memcard Pro 2 in the `import` folder. You can place OPL VMC files (.bin), PCSX2 VMC files (.ps2), MCP2 VMC files (.mc2) or save files in the  `.cbs`, `.max`, `.psu`, `.psv`, `.sps` and `.xps` formats.
 
 Open a PowerShell window in the same folder as the script, and run the command:
 
-    .\split.ps1
+    .\mcp2tools.ps1 split
 
 The script will populate the `export` folder with the newly generated files and folders for the Memcard Pro 2. Copy these folders into the "PS2" folder on the Memcard Pro 2 SD card.
+
+If you want to use a template card as a basis for all generated cards, for example one populated with an `igr` folder - you can add a `-basecard` parameter specifying a template card, for example:
+
+    .\mcp2tools.ps1 split -basecard igr.mc2
+
+A template card called `igr.mc2` has been included in the repo. It contains the file `igr/igr.elf`. In order to use this with OPL, set your `igr path` to `mc0:/igr/igr.elf` (use mc0 for slot 1, mc1 for slot 2).
+
+## Import PSU to existing VMCs
+This can be used to import a given PSU file to every `.mc2` file in the `existing_cards` folder. Begin by populating the `existing_cards` folder with the contents of your `PS2` folder from the MCP2 SD card.
+
+PSU files can be created using wLaunchELF via copy -> psuPaste on a memory card directory. A PSU file called `igr.psu` has been included, it represents the file `igr/igr.elf`.
+
+Open a PowerShell window in the same folder as the script, and run the command (for example):
+
+    .\mcp2tools.ps1 add -psu igr.psu
+
+## Remove folder from existing VMCs
+This can be used to remove a given named directory `.mc2` file in the `existing_cards` folder. Begin by populating the `existing_cards` folder with the contents of your `PS2` folder from the MCP2 SD card.
+The main purpose of this command is to remove directories you have added previously via this script, for example if they are no longer needed or to update the files. `igr` is the name of the folder added if you use the included files - if you import your own files you will need to know the name of the directory you created.
+
+Open a PowerShell window in the same folder as the script, and run the command (for example):
+
+    .\mcp2tools.ps1 remove -folder igr
---
+++ mcp2tools.ps1
@@ -0,0 +1,401 @@
+
+param(
+	[Parameter(Position=0)]
+	[string]$command="split",
+    [Parameter()]
+    [string]$basecard="blank.bin",
+	[Parameter()]
+    [string]$psu,
+	[Parameter()]
+    [string]$folder
+ )
+
+
+$myMcFolder = ".\mymc"
+$psvConverterCmd = ".\psv-converter\psv-converter-win.exe"
+$importFolder = ".\import"
+$exportFolder = ".\export"
+$existingCardsFolder = ".\existing_cards"
+$tempFolder = ".\temp"
+$cmd = "$($myMcFolder)\mymc.exe"
+$psuNameMaxLength = 32
+$gameIdRegex = 'S[A-Z]{3}-\d{5}'
+
+Function Confirm-MyMcPresent {
+	$fileExists = Test-Path $cmd
+	if (!$fileExists) {
+		Write-Output "ERROR: mymc.exe not dedected - please check the readme"
+		exit
+	}
+}
+
+Function Confirm-PsvConverterPresent {
+	return Test-Path $psvConverterCmd
+}
+
+Function Confirm-MyMcVersion {
+	$version = (& $cmd "--version" | Select-String -Pattern '2.6.g2').Matches.Value
+	if($version -ne '2.6.g2')  {
+		Write-Output "ERROR: Incorrect version of MyMc detected - please check the readme"
+		exit
+	}
+}
+
+Function Confirm-BaseCardExists {
+	$fileExists = Test-Path (Join-Path ".\" $basecard)
+	if (!$fileExists) {
+		Write-Output "ERROR: Designated base card $basecard not detected"
+		exit
+	}
+}
+
+Function Confirm-PsuFileExists {
+	if(!$psu) {
+		Write-Output "ERROR: Please specify a save to add using -psu"
+		exit
+	}
+	if(!($psu -Like "*.psu")) {
+		Write-Output "ERROR: PSU file extension incorrect - ensure the file is a .psu file"
+		exit
+	}
+
+	$fileExists = Test-Path (Join-Path ".\" $psu)
+	if (!$fileExists) {
+		Write-Output "ERROR: PSU file to add $psu not detected"
+		exit
+	}
+
+}
+
+Function Confirm-FolderToRemove {
+	if(!$folder) {
+		Write-Output "ERROR: Please specify a folder to remove using -folder"
+		exit
+	}
+}
+
+Function Confirm-FilesToImport {
+	$saveFileCount = (Get-ChildItem -Path "$($importFolder)\*" -Include ('*.psu','*.xps','*.max','*.cbs','*.sps','*.psv','*.mc2','*.ps2','*.bin') |  Measure-Object).Count
+	if ($saveFileCount -eq 0) {
+		Write-Output "No save files detected in import folder"
+		exit
+	}
+}
+
+Function Confirm-FilesInExistingCards {
+	$vmcFileCount = (Get-ChildItem -Path "$($existingCardsFolder)\*" -Include ('*.mc2') -Recurse |  Measure-Object).Count
+	if ($vmcFileCount -eq 0) {
+		Write-Output "No VMC files detected in existing_cards folder"
+		exit
+	}
+}
+
+Function Move-Mc2sToTemp {
+	$mcFiles = Get-ChildItem -Path "$($importFolder)\*" -Include *.mc2
+	foreach($mcFile in $mcFiles) {
+		Copy-Item  -Force -Path "$($importFolder)\$($mcFile.Name)" -Destination "$($tempFolder)\$($mcFile.BaseName).bin"
+	}
+}
+
+Function Rename-ExistingCardsMc2sToBin {
+	$mcFiles = Get-ChildItem -Path "$($existingCardsFolder)\*" -Include *.mc2 -Recurse
+	foreach($mcFile in $mcFiles) {
+		Rename-Item -Force -Path "$($mcFile.Directory)\$($mcFile.Name)" -NewName "$($mcFile.BaseName).bin"
+	}
+}
+
+Function Rename-ExistingCardsBinsToMc2 {
+	$mcFiles = Get-ChildItem -Path "$($existingCardsFolder)\*" -Include *.bin -Recurse
+	foreach($mcFile in $mcFiles) {
+		Rename-Item -Force -Path "$($mcFile.Directory)\$($mcFile.Name)" -NewName "$($mcFile.BaseName).mc2"
+	}
+}
+
+Function Add-PsuToExistingCards {
+	$mcFiles = Get-ChildItem -Path "$($existingCardsFolder)\*" -Include *.bin -Recurse
+	foreach($mcFile in $mcFiles) {
+		Write-Output("Adding PSU to $($mcFile.Name.Replace(".bin",".mc2"))...")
+		$fullMcFilePath = "$($mcFile.Directory)\$($mcFile.Name)"
+		$result = & $cmd $fullMcFilePath import $psu 2>&1
+		$result = [string] $result
+		if($result.indexOf("directory exists") -gt -1) {
+			Write-Output("ERROR: $($mcFile.Name.Replace(".bin",".mc2")) already contains a folder with this name")
+		}
+		if($result.indexOf("out of space") -gt -1) {
+			Write-Output("ERROR: $($mcFile.Name.Replace(".bin",".mc2")) - not enough space for file")
+		}
+	}
+}
+
+Function Remove-PsuFromExistingCards {
+	$mcFiles = Get-ChildItem -Path "$($existingCardsFolder)\*" -Include *.bin -Recurse
+	foreach($mcFile in $mcFiles) {
+		Write-Output("Removing $folder dir from $($mcFile.Name.Replace(".bin",".mc2"))...")
+		$fullMcFilePath = "$($mcFile.Directory)\$($mcFile.Name)"
+		$result = & $cmd $fullMcFilePath delete $folder 2>&1
+		$result = [string] $result
+		if($result -and $result.indexOf("directory not found") -gt -1) {
+			Write-Output("ERROR: $($mcFile.Name.Replace(".bin",".mc2")) does not have $folder folder")
+		}
+	}
+}
+
+Function Move-Ps2sToTemp {
+	$mcFiles = Get-ChildItem -Path "$($importFolder)\*" -Include *.ps2
+	foreach($mcFile in $mcFiles) {
+		
+		Copy-Item  -Force -Path (Join-Path $importFolder $mcFile.Name) -Destination  (Join-Path $tempFolder $mcFile.Name)
+	}
+}
+
+Function Move-BinsToTemp {
+	$binFiles = Get-ChildItem -Path "$($importFolder)\*" -Include *.bin
+	foreach($binFile in $binFiles) {
+		if (Test-Path -Path "$($tempFolder)\$($binFile.BaseName).bin") {
+			$filesWithMatchingName = Get-ChildItem -Path "$($tempFolder)\$($binFile.BaseName)*" -Include *.bin
+			$newName = "$($binFile.BaseName)-$($filesWithMatchingName.Count).bin"
+			Copy-Item  -Force -Path "$($importFolder)\$($binFile.Name)" -Destination "$($tempFolder)\$($newName).bin"
+		} else {
+			Copy-Item  -Force -Path "$($importFolder)\$($binFile.Name)" -Destination "$($tempFolder)\$($binFile.BaseName).bin"
+		}
+	}
+}
+
+Function Move-SaveFilesToTemp {
+	$saveFiles = Get-ChildItem -Path "$($importFolder)\*" -Include ('*.psu','*.xps','*.max','*.cbs','*.sps')
+	foreach($saveFile in $saveFiles) {
+		Copy-Item  -Force -Path (Join-Path $importFolder $saveFile.Name) -Destination (Join-Path $tempFolder $saveFile.Name)
+	}
+}
+
+
+Function Move-PsvsToTemp {
+	$psvFiles = Get-ChildItem -Path (Join-Path $importFolder "\*") -Include *.psv
+	foreach($psvFile in $psvFiles) {
+		Copy-Item  -Force -Path (Join-Path $importFolder $psvFile.Name) -Destination (Join-Path $tempFolder $psvFile.Name)
+	}
+}
+
+
+Function Convert-PsvsToPsus {
+	$psvFiles = Get-ChildItem -Path (Join-Path $tempFolder "\*") -Include *.psv
+	foreach($psvFile in $psvFiles) {
+		& $psvConverterCmd (Join-Path $psvFile.Directory $psvFile.Name)
+	}
+}
+
+
+Function Export-Psus($mcFile) {
+	$prm = "$($tempFolder)\$($mcFile.Name)", "dir"
+	$saveList = & $cmd $prm
+	if($saveList.getType().Name -eq "String") {
+		Write-Output "No saves found in $($mcFile.BaseName)"
+		continue
+	}
+	$saves = New-Object Collections.Generic.List[String]
+	for($i = 0; $i -lt $saveList.Length; $i = $i + 3) {
+		if ($saveList[$i] -match $gameIdRegex) {
+			$psuName = $saveList[$i].Substring(0, $psuNameMaxLength).Trim()
+			if($psuName.Length) {
+				$saves.Add($psuName)
+			}
+		}
+	}
+	
+	if($saves.Length) {
+		foreach($save in $saves) {
+			$sanitizedFileName = $save.Split([IO.Path]::GetInvalidFileNameChars()) -join '_'
+			$counter = 1
+			Write-Output "Found $($save) in $($mcFile.BaseName)..."
+			$fullMcFilePath = "$($tempFolder)\$($mcFile.Name)"
+			if(Test-Path -Path "$($tempFolder)\$($sanitizedFileName).psu" -PathType Leaf) {
+				while(Test-Path -Path "$($tempFolder)\$($sanitizedFileName)-$($counter).psu" -PathType Leaf) {
+					$counter++
+				}
+				$prm = $fullMcFilePath, "export", "--directory=$tempFolder", "--output-file=$($sanitizedFileName)-$($counter).psu", $save
+			}else{
+				$prm = $fullMcFilePath, "export", "--directory=$tempFolder", "--output-file=$($sanitizedFileName).psu", $save
+			}
+			& $cmd $prm
+		}
+	}
+	
+}
+
+Function Get-PsusFromBins {
+	$binFiles = Get-ChildItem -Path "$($tempFolder)\*" -Include *.bin
+
+	foreach($binFile in $binFiles) {
+		Write-Output ''
+		Export-Psus $binFile
+	}
+}
+
+Function Get-PsusFromPs2s {
+	$ps2Files = Get-ChildItem -Path "$($tempFolder)\*" -Include *.ps2
+
+	foreach($ps2File in $ps2Files) {
+		Export-Psus $ps2File
+	}
+}
+
+Function Get-PsuWithGameId($saveFile) {
+	Copy-Item -Path (Join-Path ".\" $basecard) -Destination (Join-Path $tempFolder "tempCard.bin")
+	$prm = $prm = (Join-Path $tempFolder "tempCard.bin"), "import", (Join-Path $tempFolder $($saveFile.Name))
+	& $cmd $prm
+	$newSaveFile = Get-ChildItem -Path (Join-Path $tempFolder "tempCard.bin")
+	Export-Psus($newSaveFile)
+	Remove-Item -Path (Join-Path $tempFolder "tempCard.bin")
+}
+
+Function Repair-SavesWithNoGameId {
+	$saveFiles = Get-ChildItem -Path "$($tempFolder)\*" -Include ('*.psu','*.xps','*.max','*.cbs','*.sps')
+	foreach($saveFile in $saveFiles) {
+		if(!($saveFile.BaseName -match $gameIdRegex)) {
+			Get-PsuWithGameId($saveFile)
+			Remove-Item -Path (Join-Path $saveFile.Directory $saveFile.Name)
+		}
+	}
+}
+
+Function New-CardIfNotExist {
+	Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $gameId,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $channelNum
+    )
+	if (!(Test-Path -Path (Join-Path $exportFolder $gameId))) {
+		New-Item -ItemType Directory -Force -Path (Join-Path $exportFolder $gameId)
+	}
+
+	if (!(Test-Path -Path (Join-Path $exportFolder $gameId | Join-Path -ChildPath "$($gameId)-$($channelNum).bin"))) {
+		Copy-Item -Path (Join-Path ".\" $basecard) -Destination (Join-Path $exportFolder $gameId | Join-Path -ChildPath "$($gameId)-$($channelNum).bin")
+	}
+}
+
+Function Import-FileToCard {
+	Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $gameId,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $channelNum,
+		 [Parameter(Mandatory=$true, Position=2)]
+         $saveFile
+    )
+	$prm = ".\$($exportFolder)\$($gameId)\$($gameId)-$($channelNum).bin", "import", ".\$($tempFolder)\$($saveFile.Name)"
+	$result = & $cmd $prm 2>&1
+	$result = [string] $result
+	if($result.indexOf("directory exists") -gt -1 -or $result.indexOf("out of space") -gt -1) {
+		return $false
+	} else {
+		return $result
+	}
+
+}
+
+Function New-Vmcs {
+	$saveFiles = Get-ChildItem -Path "$($tempFolder)\*" -Include ('*.psu','*.xps','*.max','*.cbs','*.sps')
+
+	foreach($saveFile in $saveFiles) {
+		$channelNum = 1
+		if($saveFile.BaseName -match $gameIdRegex) {
+			$gameId = $Matches.0
+		} else {
+			Write-Output "Could not find Game ID in $($saveFile.Name)"
+			continue
+		}
+
+		New-CardIfNotExist $gameId $channelNum
+		$result = Import-FileToCard $gameId $channelNum $saveFile
+		if($result) {
+			Write-Output $result
+			continue
+		}
+
+		for ($nextChannel = 2; $nextChannel -lt 10; $nextChannel++) {
+			if ($nextChannel -eq 9) {
+				Write-Output "Too many saves for $($saveFile.BaseName), ignoring"
+				continue
+			} 
+			New-CardIfNotExist $gameId $nextChannel
+			$result = Import-FileToCard $gameId $nextChannel $saveFile
+			if($result) {
+				Write-Output $result
+				break
+			}
+		}
+	}
+
+	$exportFiles = $saveFiles = Get-ChildItem -Recurse -Path "$($exportFolder)\*" -Include *.bin
+
+	foreach($exportFile in $exportFiles) {
+		Move-Item -Path "$($exportFile.Directory)\$($exportFile.Name)" -Destination "$($exportFile.Directory)\$($exportFile.BaseName).mc2" -Force
+	}
+}
+
+Function New-TempDir {
+	if (!(Test-Path -Path "$($tempFolder)")) {
+		New-Item -ItemType Directory -Force -Path "$($tempFolder)"
+	}
+}
+
+Function Clear-TempDir {
+	Remove-Item -Force -Recurse -Path "$($tempFolder)"
+}
+
+Function Move-FilesToTempDir {
+	Move-SaveFilesToTemp
+	Move-Mc2sToTemp
+	Move-Ps2sToTemp
+	Move-BinsToTemp
+}
+
+Function Convert-PsvFiles {
+	$psvFiles = Get-ChildItem -Path (Join-Path $importFolder "\*") -Include *.psv
+	if($psvFiles.Length -gt 0 -and !(Confirm-PsvConverterPresent)) {
+		Write-Output ".psv files ignored as psv-converter-win.exe not found - please check the readme"
+		return
+	}
+	Move-PsvsToTemp
+	Convert-PsvsToPsus
+}
+
+Function Get-Psus {
+	Get-PsusFromBins
+	Get-PsusFromPs2s
+}
+
+Confirm-MyMcPresent
+Confirm-MyMcVersion
+
+if($command -eq "split") {
+	Confirm-BaseCardExists
+	Confirm-FilesToImport
+	New-TempDir 
+	Move-FilesToTempDir
+	Convert-PsvFiles
+	Get-Psus
+	Repair-SavesWithNoGameId
+	New-Vmcs
+	Clear-TempDir
+}
+
+if($command -eq "add") {
+	Confirm-PsuFileExists
+	Confirm-FilesInExistingCards
+	Rename-ExistingCardsMc2sToBin
+	Add-PsuToExistingCards
+	Rename-ExistingCardsBinsToMc2
+}
+
+if($command -eq "remove") {
+	Confirm-FolderToRemove
+	Confirm-FilesInExistingCards
+	Rename-ExistingCardsMc2sToBin
+	Remove-PsuFromExistingCards
+	Rename-ExistingCardsBinsToMc2
+}
+
